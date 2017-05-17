1426699e973d97478a9bdab4612a098905d75055
aaliaga <a.aliaga.val@gmail.com>
2017-05-17 14:14:45 -0400

    agrega cuadro de texto y boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,15 @@ function crearLista (){ //boton con evento click para cear lista
 		var divTexto = document.createElement("textarea");//crear caja de texto
 			divTexto.setAttribute("id", "ingresaLista");
 			divTexto.setAttribute("placeholder", "Agregar lista");
-
 			document.getElementById("mostrar-lista").appendChild(divTexto);
+
+		var boton = document.createElement("button");//Se crea boton
+			boton.setAttribute("id","caja-boton");
+			boton.onclick = function(){
+ 				alert("esta vivo!");
+ 				}
+			boton.innerText = "Agregar";//texto que mostrara el boton
+			document.getElementById("mostrar-lista").appendChild(boton);//Se indica donde se quiere agregar el boton creado
 
 	});
 }
